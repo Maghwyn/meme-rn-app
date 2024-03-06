99bf4fa97be7ef6daa966a188cab1faafab30a8a
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-06 10:31:27 +0100

    fix(prettier): Prettier failed to work because of missing packages + lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,13 @@ module.exports = {
 				project: './tsconfig.json',
 			},
 			rules: {
+				'prettier/prettier': [
+					'error',
+					{
+						singleQuote: true,
+						endOfLine: 'auto',
+					},
+				],
 				'max-params': ['error', 3], // Limit the number of parameters in a function to use object instead
 				'max-lines-per-function': ['error', 70],
 				'react/destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
