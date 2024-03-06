8c3b5c6784ead53311e3f73dea37e8dcae990ffa
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-06 10:22:06 +0100

    fix(babel): The babel config prevent ios and android to build because it was missconfigured

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
-api.cache(false)
 module.exports = {
-	presets: ['module:@react-native/babel-preset', 'module:react-native-dotenv'],
+	presets: ['module:@react-native/babel-preset'],
+	plugins: [
+		[
+			'module:react-native-dotenv',
+			{
+				moduleName: '@env',
+				path: '.env',
+				blacklist: null,
+				whitelist: null,
+				safe: false,
+				allowUndefined: true,
+			},
+		],
+	],
 };
