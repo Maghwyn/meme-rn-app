ad4e7a7fb7746b7ba8d188db977512c9db3afcb2
Jerance <michael.jerance@gmail.com>
2024-03-07 18:49:51 +0100

    feat(user): Requests & request type for get me & get user by id

---
+++ src/components/user-profil/UserProfilComments.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+const UserProfilComments = () => {
+	return (
+		<ScrollView>
+			<Text>UserProfilComments</Text>
+		</ScrollView>
+	);
+};
+
+export default UserProfilComments;
---
+++ src/components/user-profil/UserProfilMemeLists.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+const UserProfilMemeLists = () => {
+	return (
+		<ScrollView>
+			<Text>Memes</Text>
+		</ScrollView>
+	);
+};
+
+export default UserProfilMemeLists;
