11a038f6c12f6a3e0c3df1be7bfad49770231883
HugoKaba <hugo.kaba@gmail.com>
2024-03-07 10:01:24 +0100

    feat(feed-view): like and comment button

--- src/screens/feed/feedScreen.tsx
+++ src/screens/feed/feedScreen.tsx
@@ -1,30 +1,51 @@
-import React, { useRef, useState } from 'react';
-import { FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+	Image,
+	KeyboardAvoidingView,
+	Platform,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import Modal from 'react-native-modal';
+import Swiper from 'react-native-swiper';
 
-interface FeedProps {
+interface FeedScreenProps {
 	navigation: any;
 }
 
-const FeedScreen: React.FC<FeedProps> = ({ navigation }) => {
-	const [commentsVisible, setCommentsVisible] = useState(false);
-	const [comments, setComments] = useState<string[]>([]);
-	const [newComment, setNewComment] = useState('');
-	const flatListRef = useRef<FlatList | null>(null);
+interface Photo {
+	id: string;
+	source: any;
+}
 
-	const data = [
-		'./../../../assets/icon.png',
-		'./../../../assets/icon.png',
-		'./../../../assets/icon.png',
-		'./../../../assets/icon.png',
-		'./../../../assets/icon.png',
-		'./../../../assets/icon.png',
+export type FeedScreen = {
+	(props: FeedScreenProps): React.JSX.Element;
+};
+
+const FeedScreen: FeedScreen = () => {
+	const [commentsVisible, setCommentsVisible] = useState(false);
+	const [comments, setComments] = useState<{ [id: string]: string[] }>({});
+	const [newComment, setNewComment] = useState('');
+	const [currentPhotoId, setCurrentPhotoId] = useState('');
+
+	const data: Photo[] = [
+		{ id: '1', source: require('./../../../assets/icon.png') },
+		{ id: '2', source: require('./../../../assets/icon.png') },
+		{ id: '3', source: require('./../../../assets/icon.png') },
+		{ id: '4', source: require('./../../../assets/icon.png') },
+		{ id: '5', source: require('./../../../assets/icon.png') },
 	];
 
 	const handleLikePress = () => {
 		// Gérer l'action du bouton "like" ici
 	};
 
-	const handleCommentPress = () => {
+	const handleCommentPress = (id: string) => {
+		setCurrentPhotoId(id);
 		setCommentsVisible(true);
 	};
 
@@ -34,76 +55,106 @@ const FeedScreen: React.FC<FeedProps> = ({ navigation }) => {
 
 	const handleCommentSubmit = () => {
 		if (newComment.trim() !== '') {
-			setComments((prevComments) => [...prevComments, newComment]);
+			setComments((prevComments) => ({
+				...prevComments,
+				[currentPhotoId]: [...(prevComments[currentPhotoId] || []), newComment],
+			}));
 			setNewComment('');
 		}
 	};
 
-	const renderItem = ({ item, index }: { item: string; index: number }) => (
-		<View style={styles.container}>
-			<Image source={{ uri: item }} style={styles.image} />
-			<View style={styles.overlay}>
-				<View style={styles.actionsContainer}>
-					<TouchableOpacity style={styles.actionButton} onPress={handleLikePress}>
-						<Text style={styles.actionButtonText}>Like</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.actionButton} onPress={handleCommentPress}>
-						<Text style={styles.actionButtonText}>Comment</Text>
-					</TouchableOpacity>
-				</View>
-
-				{commentsVisible && (
-					<View style={styles.commentsContainer}>
-						<FlatList
-							data={comments}
-							renderItem={({ item }) => (
-								<Text style={styles.commentText}>{item}</Text>
-							)}
-							keyExtractor={(item, index) => index.toString()}
-						/>
-						<TextInput
-							placeholder="Add a comment..."
-							style={styles.input}
-							value={newComment}
-							onChangeText={(text) => setNewComment(text)}
-						/>
-						<TouchableOpacity
-							style={styles.addCommentButton}
-							onPress={handleCommentSubmit}
-						>
-							<Text style={styles.addCommentButtonText}>Add Comment</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.closeButton} onPress={handleCloseComments}>
-							<Text style={styles.closeButtonText}>Close</Text>
-						</TouchableOpacity>
-					</View>
-				)}
-			</View>
-		</View>
-	);
-
 	return (
-		<FlatList
-			data={data}
-			renderItem={renderItem}
-			keyExtractor={(item, index) => index.toString()}
-			showsVerticalScrollIndicator={false}
-			pagingEnabled
-			ref={(ref) => (flatListRef.current = ref)}
-		/>
+		<KeyboardAvoidingView
+			enabled={true}
+			style={styles.container}
+			behavior={Platform.OS === 'ios' ? 'position' : undefined}
+		>
+			<Swiper
+				style={styles.wrapper}
+				loop={false}
+				showsPagination={false}
+				horizontal={false}
+				onIndexChanged={(index) => setCurrentPhotoId(data[index].id)}
+			>
+				{data.map((photo) => (
+					<View key={photo.id} style={styles.container}>
+						<Image source={photo.source} style={styles.image} />
+
+						<View style={styles.overlay}>
+							<View style={styles.actionsContainer}>
+								<TouchableOpacity
+									style={styles.actionButton}
+									onPress={handleLikePress}
+								>
+									<Text style={styles.actionButtonText}>Like</Text>
+								</TouchableOpacity>
+								<TouchableOpacity
+									style={styles.actionButton}
+									onPress={() => handleCommentPress(photo.id)}
+								>
+									<Text style={styles.actionButtonText}>Comment</Text>
+								</TouchableOpacity>
+							</View>
+
+							<Modal
+								isVisible={commentsVisible}
+								onBackdropPress={handleCloseComments}
+								style={styles.modal}
+								backdropOpacity={0}
+							>
+								<View style={styles.modalContent}>
+									<View style={styles.row}>
+										<TouchableOpacity
+											style={styles.closeButton}
+											onPress={handleCloseComments}
+										>
+											<Text style={styles.closeButtonText}>Close</Text>
+										</TouchableOpacity>
+
+										<TextInput
+											placeholder="Add a comment..."
+											style={styles.input}
+											value={newComment}
+											onChangeText={(text) => setNewComment(text)}
+										/>
+
+										<TouchableOpacity
+											style={styles.sendButton}
+											onPress={handleCommentSubmit}
+										>
+											<Text style={styles.sendButtonText}>Send</Text>
+										</TouchableOpacity>
+									</View>
+
+									<ScrollView style={styles.commentsContainer}>
+										{(comments[currentPhotoId] || []).map(
+											(comment, commentIndex) => (
+												<Text key={commentIndex} style={styles.commentText}>
+													{comment}
+												</Text>
+											),
+										)}
+									</ScrollView>
+								</View>
+							</Modal>
+						</View>
+					</View>
+				))}
+			</Swiper>
+		</KeyboardAvoidingView>
 	);
 };
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+	},
+	wrapper: {
 		backgroundColor: 'black',
 	},
 	image: {
 		flex: 1,
 		width: '100%',
-		height: '100%',
-		resizeMode: 'cover',
 	},
 	overlay: {
 		...StyleSheet.absoluteFillObject,
@@ -124,41 +175,69 @@ const styles = StyleSheet.create({
 	actionButtonText: {
 		color: 'black',
 	},
-	commentsContainer: {
-		backgroundColor: 'rgba(0, 0, 0, 0.7)',
-		padding: 16,
+	modal: {
+		margin: 0,
+		justifyContent: 'flex-end',
 	},
-	commentText: {
-		color: 'white',
-		marginBottom: 8,
+	modalContent: {
+		backgroundColor: 'white',
+		height: '50%',
+		padding: 16,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
 	},
 	input: {
+		flex: 5,
 		borderWidth: 1,
 		borderColor: 'gray',
 		borderRadius: 8,
 		padding: 8,
-		marginVertical: 8,
-		color: 'white',
+		color: 'black',
+		height: 50,
 	},
-	addCommentButton: {
+	sendButton: {
+		flex: 1,
 		backgroundColor: 'black',
 		padding: 8,
 		borderRadius: 8,
+		height: 50,
+		justifyContent: 'center',
 		alignItems: 'center',
-		marginTop: 8,
 	},
-	addCommentButtonText: {
+	sendButtonText: {
 		color: 'white',
 	},
 	closeButton: {
+		flex: 1,
 		backgroundColor: 'black',
 		padding: 8,
 		borderRadius: 8,
+		height: 50,
+		justifyContent: 'center',
 		alignItems: 'center',
-		marginTop: 8,
 	},
 	closeButtonText: {
 		color: 'white',
+	},
+	commentsContainer: {
+		maxHeight: '50%',
+	},
+	commentText: {
+		color: 'black',
+		marginBottom: 8,
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 5,
+		padding: 10,
+	},
+	row: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		alignContent: 'center',
+		margin: 20,
+		gap: 10,
 	},
 });
 
