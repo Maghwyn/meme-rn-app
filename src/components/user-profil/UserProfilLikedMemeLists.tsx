8d548a9375049443b8af91fd7413139700493c45
Jerance <michael.jerance@gmail.com>
2024-03-07 18:49:51 +0100

    feat(user): Requests & request type for get me & get user by id

---
+++ src/components/user-profil/UserProfilLikedMemeLists.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+const UserProfilLikedMemeLists = () => {
+	return (
+		<ScrollView>
+			<Text>Memes Liked</Text>
+		</ScrollView>
+	);
+};
+
+export default UserProfilLikedMemeLists;
---
+++ src/screens/profile/UserProfileStyle.tsx
@@ -0,0 +1,50 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		width: '100%',
+	},
+	containerThumbnail: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		flexDirection: 'row',
+		width: '100%',
+		padding: 20,
+	},
+	backgroundImage: {
+		width: '100%',
+		// borderRadius: 100,
+	},
+	avatar: {
+		width: 100,
+		height: 100,
+		borderRadius: 100,
+		backgroundColor: 'grey',
+		alignSelf: 'center',
+		marginTop: 80,
+		marginBottom: -40,
+	},
+	userInfo: {
+		marginTop: 30,
+		alignItems: 'center',
+	},
+	username: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginBottom: 5,
+	},
+	email: {
+		fontSize: 16,
+		color: '#555',
+		marginBottom: 5,
+	},
+
+	viewTabButtons: {
+		marginTop: 10,
+		flexDirection: 'row',
+		width: '100%',
+		justifyContent: 'space-around',
+	},
+});
