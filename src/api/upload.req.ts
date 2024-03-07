84d2c3f6db4c6fa6178b876e5d779e61ed647233
Jerance <michael.jerance@gmail.com>
2024-03-07 14:45:11 +0100

    chore(image-picker): Install dep react-native-image-picker & activate permissions

--- src/api/upload.req.ts
+++ src/api/upload.req.ts
@@ -1,7 +1,7 @@
 import { client } from '@api/network/client';
 import type { Upload } from '@api/upload.req.type';
 
-export const uploadFiles = (formData: FormData) => {
+export const uploadFile = (formData: FormData) => {
 	return client.post<Upload>('/uploads', formData, {
 		headers: {
 			'Content-Type': 'multipart/form-data',
