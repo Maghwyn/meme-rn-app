1a5cb1f1e2838e1f07a001246484aae1dad79297
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-08 02:48:24 +0100

    feat(store): Implement the logic to update reactively the like of a meme

--- src/store/reducers/memesSlice.ts
+++ src/store/reducers/memesSlice.ts
@@ -7,6 +7,12 @@ interface MemeCommentPayload {
 	comment: Comment;
 }
 
+interface MemeLikePayload {
+	id: string;
+	userId: string;
+	liked: boolean;
+}
+
 export interface MemesState {
 	value: Array<Meme>;
 }
@@ -25,6 +31,20 @@ export const memesSlice = createSlice({
 		setNewMeme: (state, action: PayloadAction<Meme>) => {
 			state.value.push(action.payload);
 		},
+		setNewLike: (state, action: PayloadAction<MemeLikePayload>) => {
+			const index = state.value.findIndex((meme) => meme.id === action.payload.id);
+			if (index === -1) return;
+
+			if (action.payload.liked) {
+				state.value[index].likes.push(action.payload.userId);
+			} else {
+				const userIdIndex = state.value[index].likes.findIndex(
+					(userId) => userId === action.payload.userId,
+				);
+				if (index === -1) return;
+				state.value[index].likes.splice(userIdIndex, 1);
+			}
+		},
 		setNewCommment: (state, action: PayloadAction<MemeCommentPayload>) => {
 			const index = state.value.findIndex((meme) => meme.id === action.payload.id);
 			if (index === -1) return;
@@ -33,6 +53,10 @@ export const memesSlice = createSlice({
 	},
 });
 
-export const { setMemes, setNewMeme, setNewCommment } = memesSlice.actions;
+export const { setMemes, setNewMeme, setNewCommment, setNewLike } = memesSlice.actions;
 export const retrieveMemes = (state: RootState) => state.memes.value;
+export const retrieveMemeComments = (state: RootState, memeId: string) => {
+	const meme = state.memes.value.find((m) => m.id === memeId);
+	return meme ? meme.comments : [];
+};
 export default memesSlice.reducer;
